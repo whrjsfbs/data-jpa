5fcf3e58dddea735da1ec40597e71487ee90fdcd
whrjsfbs <whrjsfbs@gmail.com>
2021-07-25 16:02:18 +0900

    first

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member saveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(saveMember.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(saveMember.getUsername());
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+    }
+}
---
+++ src/test/java/study/datajpa/repository/TeamRepositoryTest.java
@@ -0,0 +1,10 @@
+package study.datajpa.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeamRepositoryTest {
+
+}
